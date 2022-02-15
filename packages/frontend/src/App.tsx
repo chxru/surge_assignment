07f3e3d2c9e5711201c286fd43e44af1c090ef23
Charuka Samarakoon <charuka@protonmail.com>
2022-02-15 23:58:18 +0530

    Chakra installed

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex width="100vw" height="100vh" justify="center" alignItems="center">
+      <h1>Heyyy</h1>
+    </Flex>
   );
 }
 
