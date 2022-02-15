e1fde34ba9328ecba1ff8aa70dd6f172c5be0da3
Charuka Samarakoon <charuka@protonmail.com>
2022-02-16 00:11:17 +0530

    React router init

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+
+import IndexPage from "./pages";
 
 const App: React.FC = () => {
   return (
-    <Flex width="100vw" height="100vh" justify="center" alignItems="center">
-      <h1>Heyyy</h1>
-    </Flex>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+    </Routes>
   );
 };
 
--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import "./index.css";
@@ -8,9 +9,11 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
