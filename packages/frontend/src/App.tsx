09ee889a7a094e8486b646a95baef20ee9babd46
Charuka Samarakoon <charuka@protonmail.com>
2022-02-16 00:01:17 +0530

    Remove CRA bloat code

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
-import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <Flex width="100vw" height="100vh" justify="center" alignItems="center">
       <h1>Heyyy</h1>
     </Flex>
   );
-}
+};
 
 export default App;
