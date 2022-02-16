5d3bd580341e14167d66c8e61a84ccba1f42e973
Charuka Samarakoon <charuka@protonmail.com>
2022-02-17 04:28:15 +0530

    change pg host according to env.production

--- packages/backend/util/db.ts
+++ packages/backend/util/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const db = new Pool({
   user: process.env.PGUSER,
-  host: "0.0.0.0",
+  host: process.env.PRODUCTION ? "host.docker.internal" : "0.0.0.0",
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: parseInt(process.env.PGPORT || "3002"),
