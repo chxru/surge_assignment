fefdafffd7e15ce10774578aab99a6d50992aaad
Charuka Samarakoon <charuka@protonmail.com>
2022-02-16 20:25:22 +0530

    Add protected routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,31 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Header from "./components/header";
+
+import { useAuth } from "./contexts/auth-context";
+import { useNotify } from "./contexts/notify-context";
 
 import IndexPage from "./pages";
 import LoginPage from "./pages/login";
 import ProfilePage from "./pages/me";
 import RegisterPage from "./pages/register";
+
+const RequireAuth = ({ children }: { children: JSX.Element }) => {
+  const auth = useAuth();
+  const notify = useNotify();
+
+  // onMount
+  useEffect(() => {
+    notify.warning("You have to be logged in to see this page");
+  }, [notify]);
+
+  if (!auth.user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
 
 const App: React.FC = () => {
   return (
@@ -16,7 +35,14 @@ const App: React.FC = () => {
         <Route path="/" element={<IndexPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/me" element={<ProfilePage />} />
+        <Route
+          path="/me"
+          element={
+            <RequireAuth>
+              <ProfilePage />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </>
   );
