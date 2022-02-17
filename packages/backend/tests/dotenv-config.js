e5f851fa9936e84d804898e965964f498b5acdbf
Charuka Samarakoon <charuka@protonmail.com>
2022-02-18 05:08:36 +0530

    Config jest ts-jest

---
+++ packages/backend/tests/dotenv-config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// use to load env vars before test cases
+require("dotenv").config({
+  path: "../../.env",
+  debug: true,
+});
